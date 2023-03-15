e763d1aa427cf46b72d8588e74497f78a1b8fc46
Lambdaderuta <39528610+Lambdaderuta@users.noreply.github.com>
2023-03-15 23:19:39 +0000

    Deploying to main from @ Lambdaderuta/input-renderer@504789fb5caf189591e00be294bef884f916fd72 🚀

---
+++ docs/assets/InputRenderer.stories-08caf6d8.js
@@ -0,0 +1,42 @@
+var o=Object.defineProperty;var n=(e,r)=>o(e,"name",{value:r,configurable:!0});import{I as t,j as a}from"./InputRenderer-07a273e0.js";import"./index-4f5f27b4.js";import"./iframe-9fcd841b.js";const u={parameters:{storySource:{source:`import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { default as InputRendererComponent } from '../InputRenderer'
+
+export default {
+    title: 'InputRenderer',
+    component: InputRendererComponent,
+} as ComponentMeta<typeof InputRendererComponent>
+
+const Template: ComponentStory<typeof InputRendererComponent> = (args) => (
+    <InputRendererComponent {...args} />
+)
+
+export const InputRenderer = Template.bind({})
+
+InputRenderer.args = {
+    inputRenderMap: [
+        {
+            id: 'first_name',
+            type: 'inputText',
+            label: 'First Name',
+            defaultValue: 'Some first name',
+        },
+        {
+            id: 'last_name',
+            type: 'inputText',
+            label: 'Last Name',
+        },
+        {
+            id: 'email',
+            type: 'inputEmail',
+            label: 'Email',
+        },
+        {
+            id: 'password',
+            type: 'inputPassword',
+            label: 'Password',
+        },
+    ],
+}
+`,locationsMap:{"input-renderer":{startLoc:{col:64,line:10},endLoc:{col:1,line:12},startBody:{col:64,line:10},endBody:{col:1,line:12}}}}},title:"InputRenderer",component:t},p=n(e=>a.jsx(t,{...e}),"Template"),s=p.bind({});s.args={inputRenderMap:[{id:"first_name",type:"inputText",label:"First Name",defaultValue:"Some first name"},{id:"last_name",type:"inputText",label:"Last Name"},{id:"email",type:"inputEmail",label:"Email"},{id:"password",type:"inputPassword",label:"Password"}]};const c=["InputRenderer"];export{s as InputRenderer,c as __namedExportsOrder,u as default};
+//# sourceMappingURL=InputRenderer.stories-08caf6d8.js.map
